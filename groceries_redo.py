e9e43d924337153752c21b1c2522f6d05f5a4b04
nolin897 <nolin897@gmail.com>
2020-05-30 14:52:06 -0400

    Groceries Redo Part 2
    Forming a unique list of departments via the conditional .append method.

--- groceries_redo.py
+++ groceries_redo.py
@@ -34,6 +34,32 @@ products = [
 
 # TODO: write some Python code here to produce the desired output
 
+#
+# PRODUCTS (PART 1)
+# --------------
+# THERE ARE 20 PRODUCTS:
+# --------------
+#  + All-Seasons Salt ($4.99)
+#  + Chocolate Fudge Layer Cake ($18.50)
+#  + Chocolate Sandwich Cookies ($3.50)
+#  + Cut Russet Potatoes Steam N' Mash ($4.25)
+#  + Dry Nose Oil ($21.99)
+#  + Fresh Scent Dishwasher Cleaner ($4.99)
+#  + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+#  + Green Chile Anytime Sauce ($7.99)
+#  + Light Strawberry Blueberry Yogurt ($6.50)
+#  + Mint Chocolate Flavored Syrup ($4.50)
+#  + Overnight Diapers Size 6 ($25.50)
+#  + Peach Mango Juice ($1.99)
+#  + Pizza For One Suprema Frozen Pizza ($12.50)
+#  + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+#  + Pure Coconut Water With Orange ($3.50)
+#  + Rendered Duck Fat ($9.99)
+#  + Robust Golden Unsweetened Oolong Tea ($2.49)
+#  + Saline Nasal Mist ($16.00)
+#  + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+#  + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+
 products_count = len(products)
 
 print(type(products))
@@ -62,27 +88,46 @@ for p in sorted_products:
     price_usd = " (${0:.2f})".format(p["price"])
     print(" + " + p["name"] + price_usd)
 
+#
+#   DEPARTMENTS (PART 2)
+#
+
+# Iterate through the list of products to assemble the list of departments.
+# Use a . to invoke a method or property on/of a variable
+
+departments = []
+
+for p in products:
+    #print(p["department"])
+    #departments.append(p["department"])
+    #This will loop through each product, and for each product it will assemble the list of departments. 
+    # It will append or add the product's department to our list of departments.
+    if p["department"] not in departments:
+        departments.append(p["department"])
+        #By only conditionally appending the department to our departments list 
+        #if that product's department doesn't already exist in the list,
+        #we hope to assemble a unique list of departments.
+
+
+department_count = len(departments)
+
+print("--------------")
+print("THERE ARE " + str(department_count) + " DEPARTMENTS:")
+print("--------------")
+
+for d in departments:
+    print(d)
 
 # --------------
-# THERE ARE 20 PRODUCTS:
+# THERE ARE 10 DEPARTMENTS:
 # --------------
-#  + All-Seasons Salt ($4.99)
-#  + Chocolate Fudge Layer Cake ($18.50)
-#  + Chocolate Sandwich Cookies ($3.50)
-#  + Cut Russet Potatoes Steam N' Mash ($4.25)
-#  + Dry Nose Oil ($21.99)
-#  + Fresh Scent Dishwasher Cleaner ($4.99)
-#  + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
-#  + Green Chile Anytime Sauce ($7.99)
-#  + Light Strawberry Blueberry Yogurt ($6.50)
-#  + Mint Chocolate Flavored Syrup ($4.50)
-#  + Overnight Diapers Size 6 ($25.50)
-#  + Peach Mango Juice ($1.99)
-#  + Pizza For One Suprema Frozen Pizza ($12.50)
-#  + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
-#  + Pure Coconut Water With Orange ($3.50)
-#  + Rendered Duck Fat ($9.99)
-#  + Robust Golden Unsweetened Oolong Tea ($2.49)
-#  + Saline Nasal Mist ($16.00)
-#  + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
-#  + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+#  + Babies (1 product)
+#  + Beverages (5 products)
+#  + Dairy Eggs (1 product)
+#  + Dry Goods Pasta (1 product)
+#  + Frozen (4 products)
+#  + Household (1 product)
+#  + Meat Seafood (1 product)
+#  + Pantry (2 products)
+#  + Personal Care (2 products)
+#  + Snacks (2 products)
